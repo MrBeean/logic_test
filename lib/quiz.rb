9e9c6ea4a207e7700a36af59b05b6e470f11aeca
mrbeean_HP <MrSitiZ@yandex.ru>
2017-03-27 09:34:58 +0300

    correct interface for quiz, correct readme

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -1,13 +1,11 @@
 class Quiz
-
-  MAX_MINUTES = 8
-
-  attr_reader :score, :quiz, :waste_time
+  attr_reader :result, :waste_time
 
   def initialize(quiz)
     @waste_time = 0
     @quiz = quiz
     @score = 0
+    @result = ''
   end
 
   def self.create
@@ -38,15 +36,18 @@ class Quiz
       @waste_time = start_time - Time.now.min
     end
 
-    check_result(user_answers)
+    check_score(user_answers)
+    user_result
   end
+
+  private
 
   def ask_user
     user_input = ''
-    user_input = STDIN.gets.chomp.delete(' ') if user_input.empty?
+    STDIN.gets.chomp if user_input.empty?
   end
 
-  def check_result(user_answers)
+  def check_score(user_answers)
     user_answers.each_with_index do |answer, i|
       @score += 1 if answer == CORRECT_ANSWERS[i]
     end
@@ -56,8 +57,15 @@ class Quiz
     start_time - Time.now.min > MAX_MINUTES
   end
 
+  def user_result
+    RESULT.each do |score_range, result|
+      @result = result if score_range.include?(@score)
+    end
+  end
+
   def reset_quiz!
     @score = 0
     @waste_time = 0
+    @result = ''
   end
 end
--- main.rb
+++ main.rb
@@ -3,14 +3,12 @@ require_relative 'lib/data'
 
 username = ARGV[0] == nil ? 'Анонимус' : ARGV[0]
 puts "Привет, #{username.encode('UTF-8')}"
-puts "Тест на логику вашего мышления. У вас есть 8 минут на 12 вопросов!\n"
+puts "Тест на логику вашего мышления. У вас есть #{MAX_MINUTES} минут на 12 вопросов!\n"
 
 quiz = Quiz.create
 
+puts "В качестве ответа пишите номер варианта, можно несколько, через запятую\n\r"
 quiz.start_quiz
 
-# # puts "Вы потратили на тест: #{quiz.waste_time} мин."
-# # puts "Ваш результат:\n"
-# # RESULT.each do |score_range, result|
-# #   puts result if score_range.include?(quiz.score)
-# end
+puts "Вы потратили на тест: #{quiz.waste_time} мин."
+puts "Ваш результат:\n#{quiz.result}"
